79e9f49e233f17719dbd1d2364214962fab20db8
sergey19881218 <sergey12-2012@mail.ru>
2020-06-26 23:43:02 +0300

    Revert "Тестирую 2"
    This reverts commit 73d9fff239e50d6cd2b7df6c9e621b590b100ad0.

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -3,8 +3,6 @@ public class MyFirstProgramm {
 		System.out.println("Hello, world!");
 <<<<<<< HEAD
 		System.out.println("Proba1 ");
-		System.out.println("Proba2 ");
-		
 
 =======
 >>>>>>> parent of d5d784e... Тестирую
