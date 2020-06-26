73d9fff239e50d6cd2b7df6c9e621b590b100ad0
sergey19881218 <sergey12-2012@mail.ru>
2020-06-26 23:38:56 +0300

    Тестирую 2

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -2,6 +2,8 @@ public class MyFirstProgramm {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Proba1 ");
+		System.out.println("Proba2 ");
+		
 
 	}
 }
