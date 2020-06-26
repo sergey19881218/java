0c07abbfe331c9a0f1fcdf37f3d5cb9182d4e678
sergey19881218 <sergey12-2012@mail.ru>
2020-06-26 23:41:05 +0300

    Update MyFirstProgramm.java

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,10 +1,13 @@
 public class MyFirstProgramm {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+<<<<<<< HEAD
 		System.out.println("Proba1 ");
 		System.out.println("Proba2 ");
 		
 
+=======
+>>>>>>> parent of d5d784e... Тестирую
 	}
 }
 
