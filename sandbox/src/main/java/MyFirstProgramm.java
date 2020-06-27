44e9b1feda34c07d7bcb8acb699b2e03231cb73f
sergey19881218 <sergey12-2012@mail.ru>
2020-06-27 10:19:23 +0300

    Revert "Привет мир"
    This reverts commit cea87d9b854a2667730beb9d8e1671218e689da0.

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -2,7 +2,6 @@ public class MyFirstProgramm {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Proba1 ");
-		System.out.println("Привет мир!");
 	}
 }
 
