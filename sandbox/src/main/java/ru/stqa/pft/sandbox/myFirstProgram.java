0f8b432d1e7681571535762b94f957fdd262d6c2
sergey19881218 <sergey12-2012@mail.ru>
2020-06-27 18:07:15 +0300

    делаем 1.7

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -1,9 +1,25 @@
 package ru.stqa.pft.sandbox;
 
-public class myFirstProgram {
-	public static void main(String[] args) {
+import com.sun.org.apache.xpath.internal.objects.XString;
 
-	    System.out.println("Hello, world!");
+public class myFirstProgram {
+
+	public static void main(String[] args) {
+	    hello("world");
+	    hello("User");
+	    hello("Alex");
+	    double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
 	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
 }
 
