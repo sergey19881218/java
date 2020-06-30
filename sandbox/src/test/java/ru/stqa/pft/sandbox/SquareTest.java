0df717bac476bac03303f70c25e38c44de508049
sergey19881218 <sergey12-2012@mail.ru>
2020-06-30 18:17:47 +0300

    Лекция 1 End

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTest {
@@ -7,6 +8,6 @@ public class SquareTest {
     @Test
     public void testArea() {
         Square s = new Square(5);
-        assert s.area() == 25;
+        Assert.assertEquals(s.area(), 25.0);
     }
 }
